1121b501adb88e261fbd3032ab60c577d88c91b5
jose <jose@voris.co.in>
2017-06-05 15:24:33 +0530

    setuped server and basic routes

--- server.js
+++ server.js
@@ -1,1 +1,43 @@
-server.js
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
+
+
+var port = 3000;
+var app = express();
+
+
+// view engine
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
+
+
+
+// Set static folder
+
+app.use(express.static(path.join(__dirname,'client')));
+
+
+
+// Body parser middileware
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+
+app.use('/',index);
+app.use('/api',tasks);
+
+app.listen(port, function() {
+	console.log('server running '+port);
+});
+
+
+
+
