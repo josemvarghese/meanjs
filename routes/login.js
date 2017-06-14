f119e67c2d9a49fcc62d3d388a38f4d53b9eb12f
jose <jose@voris.co.in>
2017-06-14 19:15:07 +0530

    password hash started - not working

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
 var mongojs = require('mongojs');
 // mongo db collection, collections that listed array can use here
  /*  collections are basically tables in mongodb */
 var db = mongojs('mongodb://jose:jose@ds025603.mlab.com:25603/tasks',['user']);
-
+const saltRounds = 10;
+const myPlaintextPassword = 'jose';
+const someOtherPlaintextPassword = 'not_bacon';
 
 
 router.post('/signup',function(req,res,next) {
@@ -16,12 +19,20 @@ router.post('/signup',function(req,res,next) {
 	// 	res.status(400)
 	// }
 	// else{
+		// bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+		// 	console.log(myPlaintextPassword);
+		// 	console.log(hash);
+		// // Store hash in your password DB.
+		// });
+
 		db.user.save(userInfo,function(err,user){
 			if(err){
 				res.send(err);
 			}
 			res.json(user);
 		});
+
+
 	// }
 
 });
